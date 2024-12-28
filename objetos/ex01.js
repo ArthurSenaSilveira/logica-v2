6927bb26427a11a9d80c918c2ea34531fd01327b
Arthur Henrique Sena Silveira <arthurhsesi@gmail.com>
2024-12-28 01:24:38 -0300

    Primeiro exercício da sessão de objetos do curso concluído

---
+++ objetos/ex01.js
@@ -0,0 +1,8 @@
+const carro = {
+    modelo: "Uno",
+    ano: 2023,
+    km: 10000,
+    combustivel: "Gasolina",
+    listrosConsumidos: 625
+}
+console.log(`O carro ${carro.modelo} ${carro.ano} fez em média ${carro.km / carro.listrosConsumidos} Km/L de galolina`)
--- objetos/introducao.js
+++ objetos/introducao.js
@@ -2,6 +2,5 @@ const freela = {
     nome: 'Sofá',
     preco: 1000,
     quantidade: 3,
-    estoque: 0,
 }
-console.log(`O preço do ${freela.nome} é ${freela.preco} Reais, temos ${freela.quantidade} disponíveis, no estoque temos ${freela.estoque}`)
+console.log(`${freela.nome} custa R$${freela.preco} e há ${freela.quantidade} unidades no estoque`)
