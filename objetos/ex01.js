b77947644a22bc770f1121dec1d8a762d154fcb9
Arthur Henrique Sena Silveira <arthurhsesi@gmail.com>
2024-12-28 01:32:58 -0300

    ajustando o exercício

--- objetos/ex01.js
+++ objetos/ex01.js
@@ -2,7 +2,9 @@ const carro = {
     modelo: "Uno",
     ano: 2023,
     km: 10000,
-    combustivel: "Gasolina",
+    combustivel: "gasolina",
     listrosConsumidos: 625
 }
-console.log(`O carro ${carro.modelo} ${carro.ano} fez em média ${carro.km / carro.listrosConsumidos} Km/L de galolina`)
+carro.marca = "Fiat"
+
+console.log(`O carro ${carro.marca} ${carro.modelo} ano de ${carro.ano} fez em média ${carro.km / carro.listrosConsumidos}Km/L de ${carro.combustivel}`)
